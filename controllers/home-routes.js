2c79a71fbd9553c2a378645dd707b18e9a62ab18
Sophoan Meas <sophoan.meas@hotmail.com>
2022-05-26 11:44:33 -0400

    add classes

---
+++ controllers/home-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+// TODO: Import the custom middleware
+
+// GET all galleries for homepage
+router.get('/', async (req, res) => {
+
+    res.send('Welcome to Quotes-Smith Homepage');
+  
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const apiRoute = require('./api')
+// const apiRoute = require('./api')
 const homeRoutes = require('./home-routes')
 
-router.use('/`', homeRoutes);
-router.use('/api', apiRoutes)
+router.use('/', homeRoutes);
+// router.use('/api', apiRoutes)
 
 module.exports = router;
