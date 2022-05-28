0917fb56f6cc226bc9b00128ca0d6dd74fb30bb7
Sophoan Meas <sophoan.meas@hotmail.com>
2022-05-27 21:19:43 -0400

    add login/logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,44 +1,61 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// GET ALL USERS - (not to be used other than testing in insomnia)
-router.get('/all', (req, res) => {
-  // Access our User model and run .findAll() method)
-  User.findAll({
-    // attributes: { exclude: ['password'] }
-  })
-    .then(userData => res.json(userData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+// Login
+router.post('/login', async(req, res) =>{
+  try {
+    const userData = await User.findOne({
+      where:{
+        username: req.body.username
+      }
+    })
+    if (!userData){
+      res.status(400).json({ message: 'Wrong username/password. Try again!'})
+      return;
+    }
+    const password = await userData.checkPassword(req.body.password)
+    if (!password){
+      req.session.loggedIn = true;
+      res.status(200).json({ user: userData, message: 'Your are logged in'})
+    }
+  }catch(err){
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+// Logout
+router.post('/logout', async(req, res)=>{
+  if (req.session.loggedIn){
+    req.session.destroy(()=>{
+      res.status(204).end()
+    })
+  }else{
+    res.status(404).end();
+  }
+})
+
+
+// CREATE new user
+router.post('/', async (req, res) => {
+	try {
+		const userData = await User.create({
+			username: req.body.username,
+			first_name: req.body.first_name,
+			last_name: req.body.last_name,
+			email: req.body.email,
+			password: req.body.password
+		});
+
+		req.session.save(() => {
+			req.session.loggedIn = true;
+			res.status(200).json(userData);
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 });
 
-
-// // POST a user (CREATE A USER)
-// router.post('/', (req, res) => {
-//   User.create({
-//     username: req.body.username,
-//     first_name: req.body.first_name,
-//     last_name: req.body.last_name,
-//     email: req.body.email,
-//     password: req.body.password
-//   })
-//     .then(quoteData => res.json(quoteData))
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-
-// User routes
-// http://localhost:3006/api/users
-router.get('/', async (req, res) => {
-
-  res.send('User section');
-
-});
 
 module.exports = router;
-
