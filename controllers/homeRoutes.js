41713746edd4222112afbe25dd83661a05b0fd43
IsaiasQB <isaias.qb@hotmail.com>
2022-05-30 14:47:22 -0400

    working on homepage quote of the day

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,38 +1,68 @@
 const router = require('express').Router();
-const { Quotes, User } = require('../models');
+const { Quotes, User, Category } = require('../models');
+const sequelize = require('../config/connection')
 // TODO: Import the custom middleware
 
 // GET Quotes of the Day
-router.get('/', async (req, res) => {
-	try {
-		const quotesData = await Quotes.findAll({
-            include: [
-                {
-                  model: User,
-                  attributes: ['username', 'first_name'],
-                },
-              ],
+// router.get('/', async (req, res) => {
+// 	try {
+// 		const quotesData = await Quotes.findAll({
+//             include: [
+//                 {
+//                   model: User,
+//                   attributes: ['username', 'first_name'],
+//                 },
+//               ],
            
-		});
+// 		});
 
-		const quotes = quotesData.map((quote) => quote.get({ plain: true }));
-		res.render('homepage', {
-			quotes
-			// loggedIn: req.session.loggedIn,
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+// 		const quotes = quotesData.map((quote) => quote.get({ plain: true }));
+// 		res.render('homepage', {
+// 			quotes
+// 			// loggedIn: req.session.loggedIn,
+// 		});
+// 	} catch (err) {
+// 		console.log(err);
+// 		res.status(500).json(err);
+// 	}
+// });
+
+// router.get('/login', (req, res) => {
+// 	if (req.session.loggedIn) {
+// 		res.redirect('/');
+// 		return;
+// 	}
+
+// 	res.render('login');
+// });
+
+//FROM ADRIAN BRANCH=======================================
+router.get('/', (req, res) => {
+	Quotes.findAll({
+    attributes: ['id', 'description', 'author', 'likes'],
+    order: sequelize.literal('rand()'), 
+    limit: 1,
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      },
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+    ]
+  })
+    .then(quoteData => {
+			console.log(quoteData);
+			const quotes = quoteData.map(post => post.get({ plain: true }));
+			res.render('homepage', { quotes });
+		})
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
-
-router.get('/login', (req, res) => {
-	if (req.session.loggedIn) {
-		res.redirect('/');
-		return;
-	}
-
-	res.render('login');
-});
+//FROM ADRIAN BRANCH=======================================
 
 module.exports = router;
